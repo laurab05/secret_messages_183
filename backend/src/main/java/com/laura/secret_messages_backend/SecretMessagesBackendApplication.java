182a4ae6de6e7035f1810f30d0d062a74eecc96d
laurab05 <145872668+laurab05@users.noreply.github.com>
2026-06-17 22:11:05 +0200

    added dependencies

---
+++ backend/src/main/java/com/laura/secret_messages_backend/SecretMessagesBackendApplication.java
@@ -0,0 +1,13 @@
+package com.laura.secret_messages_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecretMessagesBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecretMessagesBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/laura/secret_messages_backend/SecretMessagesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laura.secret_messages_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecretMessagesBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
