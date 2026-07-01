b0f1d1b0f8e95f5e32565987ccc313cf75791c2c
laurab05 <145872668+laurab05@users.noreply.github.com>
2026-07-01 20:15:26 +0200

    finished user model

---
+++ backend/src/main/java/com/laura/secret_messages_backend/model/User.java
@@ -0,0 +1,23 @@
+package com.laura.secret_messages_backend.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@Entity
+
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id; 
+
+    @Column(unique = true)
+    private String username;
+    private String password;
+}
